779347ef9fa0410e432758bb0f778006d52918cf
anna <annapiwonska1@gmail.com>
2020-02-21 11:22:06 +0100

    Create basic main page view

--- src/pages/mainPage/index.js
+++ src/pages/mainPage/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import { 
+  ContainerDiv,
+  CategoryContainer
+} from './style';
+
+const categories = [
+  {id:1, name:'announcment'}, 
+  {id:2, name:'general'}, 
+  {id:3, name:'other topics'}
+];
+
+const categoryList = categories.map(category => {
+  return (
+    <CategoryContainer key={category.id}>
+      {category.name}
+    </CategoryContainer>
+  )
+})
+
 const MainPage = () => {
   return (
-    <div>
-      MainPage
-    </div>
+    <ContainerDiv>
+      {categoryList}
+    </ContainerDiv>
   )
 }
 
---
+++ src/pages/mainPage/style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+import theme from '../../layout/utils/theme';
+
+export const ContainerDiv = styled.div`
+  max-width: 1000px;
+  width: 100%;
+`;
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 15px auto;
+  padding: 0 20px;
+  min-height: 50px;
+  width: 90%;
+  max-width: 600px;
+  border: 2px solid ${theme.colors.main};
+  border-radius: ${theme.radius.bg};
+  box-shadow: 0 0 4px ${theme.colors.neutralMidLight};
+  text-transform: capitalize;
+`;
