0ffd6fcbf0dc73b0ce46cf03fe715c79fd3437d8
anna <annapiwonska1@gmail.com>
2020-02-21 16:10:09 +0100

    Add action fetchCategory

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,18 @@
 import forum from '../../apis/forum';
 
 import {
-  FETCH_CATEGORIES
+  FETCH_CATEGORIES,
+  FETCH_CATEGORY
 } from './types';
 
 export const fetchCategories = () => async dispatch => {
   const response = await forum.get('/categories');
 
   dispatch({ type: FETCH_CATEGORIES, payload: response.data})
+}
+
+export const fetchCategory = (id) => async dispatch => {
+  const response = await forum.get(`/categories/${id}`);
+
+  dispatch({ type: FETCH_CATEGORY, payload: response.data})
 }
--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,1 +1,2 @@
 export const FETCH_CATEGORIES = 'fetch_categories';
+export const FETCH_CATEGORY = 'fetch_category';
