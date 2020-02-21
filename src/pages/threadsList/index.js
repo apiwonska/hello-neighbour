ff4c605b11800696d8d77dc1bc406ac17c94aa92
anna <annapiwonska1@gmail.com>
2020-02-21 16:13:24 +0100

    Connect redux to ThreadList component and create basic component structure

--- src/pages/threadsList/index.js
+++ src/pages/threadsList/index.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const ThreadsList = () => {
-  return (
+import { fetchCategory } from '../../redux/actions';
+import { 
+
+} from './style.js';
+
+class ThreadsList extends React.Component {
+  componentDidMount() {
+    this.props.fetchCategory(this.props.match.params.categoryId)
+  }
+
+  renderThreadsList() {
+    const threadsList = this.props.category.threads.map(thread => {
+      return (
+        <div key={thread.id}>
+          <h5>{ thread.title }</h5>
+          <span>mesg: 10</span>
+          <span>Added: { thread.created }</span>
+          <span>Last post: { thread.updated }</span>
+        </div>
+      )
+    })
+    return threadsList;
+  }
+
+  render() {
+    if(!Object.keys(this.props.category).length) {
+      return <div>Loading...</div>
+    }
+
+    return (
     <div>
-      ThreadsList
+      <h2>{this.props.category.name}</h2>
+      <button>Add Thread</button>
+      <div>
+        { this.renderThreadsList() }
+      </div>      
     </div>
   )
+  }  
 }
 
-export default ThreadsList;
+const mapStateToProps = state => {  
+  return {
+    category: state.category
+  }
+}
+
+export default connect(mapStateToProps, {fetchCategory})(ThreadsList);
