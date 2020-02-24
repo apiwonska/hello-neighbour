7a771139ef747e278640205e74b0e85b68569bfd
anna <annapiwonska1@gmail.com>
2020-02-24 10:30:33 +0100

    Create Thread component structure and basic styling

--- src/pages/thread/index.js
+++ src/pages/thread/index.js
@@ -1,11 +1,85 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Thread = () => {
+import { fetchThread } from '../../redux/actions'
+import { ContainerDiv } from '../../components/common/styledDivs';
+import {
+  LinkWrapper,
+  NavLink,
+  FirstPostWrapper,  
+  PostWrapper,
+  AddPostForm,
+  UserName,
+  ThreadTitle,
+  Content,
+  Footer,
+  DateSpan,
+} from './style'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faUser } from '@fortawesome/free-solid-svg-icons'
+
+class Thread extends React.Component {
+  componentDidMount() {
+    this.props.fetchThread(this.props.match.params.threadId);
+  }
+
+  renderPosts() {
+    const postsList = this.props.thread.posts.map(post => {
+      return (
+        <PostWrapper key={ post.id }>
+          <UserName>
+            <FontAwesomeIcon icon={ faUser}/>{ post.userName }
+          </UserName>
+          <ThreadTitle>{ post.title }</ThreadTitle>
+          <Content>{ post.content }</Content>
+          <Footer>
+            <DateSpan>{ post.updated || post.created}</DateSpan>
+          </Footer>
+        </PostWrapper>
+      )
+    })    
+    return postsList;
+  }
+
+  render() {
+    if(!Object.keys(this.props.thread).length) {
+      return <div>Loading...</div>
+    }
+    return (
+      <ContainerDiv>
+        <LinkWrapper>
+          <FontAwesomeIcon icon={faArrowLeft}/>&nbsp;
+          <NavLink to={`/categories/${this.props.match.params.categoryId}`}>Back to all threads</NavLink>
+        </LinkWrapper>
+
+        <FirstPostWrapper>
+          <UserName>
+            <FontAwesomeIcon icon={ faUser}/>{ this.props.thread.userName }
+          </UserName>
+          <ThreadTitle>{ this.props.thread.title }</ThreadTitle>
+          <Content>{ this.props.thread.content }</Content>
+          <Footer>
+            <DateSpan>
+              { this.props.thread.updated || this.props.thread.created }
+            </DateSpan>
+          </Footer>
+        </FirstPostWrapper>
+
+        {this.renderPosts()}       
+
+        <AddPostForm>
+        </AddPostForm>
+      </ContainerDiv>
+    )
+  }  
+}
+
+const mapStateToProps = state => {
   return (
-    <div>
-      Thread
-    </div>
+    { 
+      thread: state.thread,
+    }
   )
 }
 
-export default Thread;
+export default connect(mapStateToProps, {fetchThread})(Thread);
---
+++ src/pages/thread/style.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import theme from '../../layout/utils/theme';
+
+export const LinkWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const NavLink = styled(Link)`
+  color: ${theme.colors.black};
+  text-decoration: underline;
+`;
+
+export const FirstPostWrapper = styled.div`
+  background-color: ${ theme.colors.neutralExtraLight };  
+  padding: 5px;
+  margin-bottom: 20px;
+  box-shadow: 0 0 4px ${ theme.colors.neutralMidLight };
+  border-radius: ${ theme.radius.sm };
+`;
+
+export const PostWrapper = styled.div`
+  background-color: ${ theme.colors.white };  
+  padding: 5px;
+  margin-bottom: 20px;
+  box-shadow: 0 0 4px ${ theme.colors.neutralMidLight };
+  border-radius: ${ theme.radius.sm };
+`;
+
+export const AddPostForm = styled.div``;
+
+export const UserName = styled(Link)`
+  margin: 5px 0 10px;
+  font-weight: 600;
+  color: ${theme.colors.black};
+`;
+
+export const ThreadTitle = styled.h4`
+  margin: 5px 0 10px;
+  font-weight: 600;
+`;
+
+export const Content = styled.div`
+  font-size: 16px;
+  margin-bottom: 10px;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+`;
+
+export const DateSpan = styled.span`
+  font-size: 14px;
+  color: ${ theme.colors.neutralMidDark};
+`;
