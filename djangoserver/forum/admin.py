48141d432c6fdb73a5c6818295fe016bbceeb792
anna <annapiwonska1@gmail.com>
2020-02-27 15:53:42 +0100

    Creates category model

--- djangoserver/forum/admin.py
+++ djangoserver/forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Category)
--- djangoserver/forum/models.py
+++ djangoserver/forum/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+"""Contains models for category, thread and post."""
 
-# Create your models here.
+from django.db import models
+# from django.contrib.auth.models import User
+
+class Category(models.Model):
+    """Category model."""
+
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(max_length=100)
+    order = models.PositiveSmallIntegerField(null=True, blank=True, verbose_name='display order')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order']
+        
+    def __str__(self):
+        return self.name
