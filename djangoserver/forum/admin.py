844420666e6dc7539c2a67240ab7a4925d353928
anna <annapiwonska1@gmail.com>
2020-03-01 01:17:24 +0100

    Register models in admin panel

--- djangoserver/forum/admin.py
+++ djangoserver/forum/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Thread, Post
 
+
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
+    '''Admin View for Category'''
 
-admin.site.register(Category)
+    list_display = ('__str__', 'threads', 'posts', 'created')
+    readonly_fields = ('created', 'updated', 'threads', 'posts')
+
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    '''Admin View for Thread'''
+
+    list_display = ('__str__', 'user', 'category', 'posts', 'created')
+    list_filter = ('sticky', 'closed')
+    readonly_fields = ('created', 'updated', 'posts')
+    search_fields = ('title', 'subject', 'category')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    '''Admin View for Post'''
+
+    list_display = ('__str__', 'user', 'thread', 'created')
+    readonly_fields = ('created', 'updated')
+    search_fields = ('content', 'user', 'thread')
