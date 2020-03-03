050b73de7cdef5ffb74e61b3d582de2a8d5218d9
anna <annapiwonska1@gmail.com>
2020-03-03 21:54:58 +0100

    Create serializers

---
+++ djangoserver/forum/api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from forum.models import Category, Thread, Post
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'threads', 'posts']
+
+
+class ThreadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Thread
+        fields = ['id', 'title', 'subject', 'user', 'category', 'sticky', 'closed', 'posts', 'latest_post_time', 'created', 'updated']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'content', 'user', 'thread', 'views', 'created', 'updated']
