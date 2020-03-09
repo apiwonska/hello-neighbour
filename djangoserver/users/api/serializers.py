74d67ff2c7017183d324c0b82363e6c1501e37fc
anna <annapiwonska1@gmail.com>
2020-03-09 16:16:38 +0100

    Add user serializers

--- djangoserver/users/api/serializers.py
+++ djangoserver/users/api/serializers.py
@@ -3,15 +3,16 @@ from rest_framework import serializers
 from users.models import CustomUser
 
 
-class LoginSerializer(serializers.ModelSerializer):
+class UserPublicSerializer(serializers.ModelSerializer):
+    """Serialize user data that is meant to be visible to other users.
+    """
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'password']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+        fields = ['id', 'username', 'date_joined', 'status', 'description']
 
-class UserSerializer(serializers.ModelSerializer):
+class UserPrivateSerializer(serializers.ModelSerializer):
+    """Serialize user data that is meant to be visible only to a profile owner.
+    """
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'first_name', 'last_name', 'date_joined', 'last_login']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'date_joined', 'status', 'description']
