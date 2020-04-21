c13935549d9225f1a122b6186cb390d5379f79a8
anna <annapiwonska1@gmail.com>
2020-04-22 00:54:45 +0200

    Add authorization header and delete baseURL

--- forumproject/forumproject-ui/src/apis/forum.js
+++ forumproject/forumproject-ui/src/apis/forum.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+import store from '../store';
+
+const state = store.getState();
+const token = state.token;
+const auth_header = token ? `Token ${token}` : '';
+
 export default axios.create({
-  baseURL: 'http://localhost:3001'
+  headers: {
+    'Authorization': auth_header
+  }
 });
