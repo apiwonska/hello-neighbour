38d1fe78bd1466686a5daa16f4e3812904a2828b
anna <annapiwonska1@gmail.com>
2020-04-22 01:07:00 +0200

    Add categoryThreads and threadPosts reducers.

--- forumproject/forumproject-ui/src/redux/reducers/index.js
+++ forumproject/forumproject-ui/src/redux/reducers/index.js
@@ -2,15 +2,18 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import categoriesReducer from './categoriesReducer';
-import categoryReducer from './categoryReducer';
+import categoryThreadsReducer from './categoryThreadsReducer';
+import threadPostsReducer from './threadPostsReducer';
 import threadReducer from './threadReducer';
+
 
 const reducers = combineReducers({
   isSignedIn: () => true,
   token: () => '3df1f99d17fac0685dd37ec04bf2b14a012de3d7',
   categories: categoriesReducer,
-  category: categoryReducer,
+  categoryThreads: categoryThreadsReducer,
   thread: threadReducer,
+  threadPosts: threadPostsReducer,
   form: formReducer,
 });
 
---
+++ forumproject/forumproject-ui/src/redux/reducers/threadPostsReducer.js
@@ -0,0 +1,14 @@
+import {
+  FETCH_THREAD
+} from '../actions/types';
+
+const reducer = (state={}, action) => {  
+  switch (action.type) {
+    case FETCH_THREAD:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
