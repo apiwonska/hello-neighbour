757f15c8c90ea2a959f0d25d17873f3082f33a3e
anna <annapiwonska1@gmail.com>
2020-04-22 01:01:36 +0200

    change api URLs and add action fetchThreadPosts

--- forumproject/forumproject-ui/src/redux/actions/index.js
+++ forumproject/forumproject-ui/src/redux/actions/index.js
@@ -1,31 +1,37 @@
 import forum from '../../apis/forum';
+// import forum1 from '../../apis/forum1';
 
 import {
   FETCH_CATEGORIES,
-  FETCH_CATEGORY,
+  FETCH_CATEGORY_THREADS,
   FETCH_THREAD,
+  FETCH_THREAD_POSTS,
   // CREATE_POST
 } from './types';
 
 export const fetchCategories = () => async dispatch => {
-  const response = await forum.get('/categories');
+  const response = await forum.get('http://localhost:8000/api/categories');
 
   dispatch({ type: FETCH_CATEGORIES, payload: response.data})
 }
 
-export const fetchCategory = (categoryId) => async dispatch => {
-  const response = await forum.get(`/categories/${categoryId}`);
+export const fetchCategoryThreads = (categoryId) => async dispatch => {
+  const response = await forum.get(`http://localhost:8000/api/threads/?category=${categoryId}`);
 
-  dispatch({ type: FETCH_CATEGORY, payload: response.data})
+  dispatch({ type: FETCH_CATEGORY_THREADS, payload: response.data})
 }
 
 export const fetchThread = (threadId) => async dispatch => {
-  const response = await forum.get(`/threads/${threadId}`);
+  // const response = await forum1.get(`/threads/${threadId}`);
+  const response = await forum.get(`http://localhost:8000/api/threads/${threadId}`);
 
-  dispatch({
-    type: FETCH_THREAD,
-    payload: response.data
-  })
+  dispatch({ type: FETCH_THREAD, payload: response.data })
+}
+
+export const fetchThreadPosts = (threadId) => async dispatch => {
+  const response = await forum.get(`http://localhost:8000/api/posts/?thread=${threadId}`);
+
+  dispatch({ type: FETCH_THREAD_POSTS, payload: response.data })
 }
 
 // export const createPost = (formValues, threadId, userId) => async dispatch => {
--- forumproject/forumproject-ui/src/redux/actions/types.js
+++ forumproject/forumproject-ui/src/redux/actions/types.js
@@ -1,4 +1,5 @@
 export const FETCH_CATEGORIES = 'fetch_categories';
-export const FETCH_CATEGORY = 'fetch_category';
+export const FETCH_CATEGORY_THREADS = 'fetch_category_threads';
 export const FETCH_THREAD = 'fetch_thread';
+export const FETCH_THREAD_POSTS = 'fetch_thread_posts';
 export const CREATE_POST = 'create_post';
