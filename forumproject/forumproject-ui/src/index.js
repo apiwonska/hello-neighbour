5d61fd968900071206db6788b0aff92126daa187
anna <annapiwonska1@gmail.com>
2020-04-21 14:46:43 +0200

    Move redux store to separate file

--- forumproject/forumproject-ui/src/index.js
+++ forumproject/forumproject-ui/src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 
-import reducers from './redux/reducers';
+import store from './store';
 import Layout from './layout/Layout';
 import Routing from './Routing';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(thunk))
-);
 
 class App extends React.Component{
   render() {
