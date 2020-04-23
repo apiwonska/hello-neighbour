d0a5cfa8aca1743f48e8c83d54cb13f302837eac
anna <annapiwonska1@gmail.com>
2020-04-23 10:38:40 +0200

    Add action types and remain existing types

--- forumproject/forumproject-ui/src/redux/actions/types.js
+++ forumproject/forumproject-ui/src/redux/actions/types.js
@@ -1,5 +1,17 @@
-export const FETCH_CATEGORIES = 'fetch_categories';
-export const FETCH_CATEGORY_THREADS = 'fetch_category_threads';
-export const FETCH_THREAD = 'fetch_thread';
-export const FETCH_THREAD_POSTS = 'fetch_thread_posts';
+export const FETCH_CATEGORIES_PENDING = 'fetch_categories_pending';
+export const FETCH_CATEGORIES_FULFILLED = 'fetch_categories_fulfilled';
+export const FETCH_CATEGORIES_ERRORS = 'fetch_categories_errors';
+
+export const FETCH_THREADS_BY_CATEGORY_PENDING = 'fetch_threads_by_category_pending';
+export const FETCH_THREADS_BY_CATEGORY_FULFILLED = 'fetch_threads_by_category_fulfilled';
+export const FETCH_THREADS_BY_CATEGORY_ERRORS = 'fetch_threads_by_category_errors';
+
+export const FETCH_THREAD_PENDING = 'fetch_thread_pending';
+export const FETCH_THREAD_FULFILLED = 'fetch_thread_fulfilled';
+export const FETCH_THREAD_ERRORS = 'fetch_thread_errors';
+
+export const FETCH_POSTS_BY_THREAD_PENDING = 'fetch_posts_by_thread_pending';
+export const FETCH_POSTS_BY_THREAD_FULFILLED = 'fetch_posts_by_thread_fulfilled';
+export const FETCH_POSTS_BY_THREAD_ERRORS = 'fetch_posts_by_thread_errors';
+
 export const CREATE_POST = 'create_post';
