30f256109a60697be553056992b36666a2b2e36a
anna <annapiwonska1@gmail.com>
2020-04-23 10:40:09 +0200

    Redo actions to consider error handling and fetch status

--- forumproject/forumproject-ui/src/redux/actions/index.js
+++ forumproject/forumproject-ui/src/redux/actions/index.js
@@ -1,37 +1,59 @@
 import forum from '../../apis/forum';
-// import forum1 from '../../apis/forum1';
 
 import {
-  FETCH_CATEGORIES,
-  FETCH_CATEGORY_THREADS,
-  FETCH_THREAD,
-  FETCH_THREAD_POSTS,
+  FETCH_CATEGORIES_PENDING,
+  FETCH_CATEGORIES_FULFILLED,
+  FETCH_CATEGORIES_ERRORS,
+  FETCH_THREADS_BY_CATEGORY_PENDING,
+  FETCH_THREADS_BY_CATEGORY_FULFILLED,
+  FETCH_THREADS_BY_CATEGORY_ERRORS,
+  FETCH_THREAD_PENDING,
+  FETCH_THREAD_FULFILLED,
+  FETCH_THREAD_ERRORS,
+  FETCH_POSTS_BY_THREAD_PENDING,
+  FETCH_POSTS_BY_THREAD_FULFILLED,
+  FETCH_POSTS_BY_THREAD_ERRORS,
   // CREATE_POST
 } from './types';
 
 export const fetchCategories = () => async dispatch => {
-  const response = await forum.get('http://localhost:8000/api/categories');
-
-  dispatch({ type: FETCH_CATEGORIES, payload: response.data})
+  dispatch({ type: FETCH_CATEGORIES_PENDING })
+  try {
+    const response = await forum.get(`http://localhost:8000/api/categories`);
+    dispatch({ type: FETCH_CATEGORIES_FULFILLED, payload: response.data })    
+  } catch(err) {
+    dispatch({ type: FETCH_CATEGORIES_ERRORS, payload: err })
+  }
 }
 
-export const fetchCategoryThreads = (categoryId) => async dispatch => {
-  const response = await forum.get(`http://localhost:8000/api/threads/?category=${categoryId}`);
-
-  dispatch({ type: FETCH_CATEGORY_THREADS, payload: response.data})
+export const fetchThreadsByCategory = (categoryId) => async dispatch => {
+  dispatch({ type: FETCH_THREADS_BY_CATEGORY_PENDING })
+  try {
+    const response = await forum.get(`http://localhost:8000/api/threads/?category=${categoryId}`);
+    dispatch({ type: FETCH_THREADS_BY_CATEGORY_FULFILLED, payload: response.data })
+  } catch(err) {
+    dispatch({ type: FETCH_THREADS_BY_CATEGORY_ERRORS, payload: err })
+  }
 }
 
 export const fetchThread = (threadId) => async dispatch => {
-  // const response = await forum1.get(`/threads/${threadId}`);
-  const response = await forum.get(`http://localhost:8000/api/threads/${threadId}`);
-
-  dispatch({ type: FETCH_THREAD, payload: response.data })
+  dispatch({ type: FETCH_THREAD_PENDING })
+  try {
+    const response = await forum.get(`http://localhost:8000/api/threads/${threadId}`);
+    dispatch({ type: FETCH_THREAD_FULFILLED, payload: response.data })    
+  } catch(err) {
+    dispatch({ type: FETCH_THREAD_ERRORS, payload: err })
+  }
 }
 
-export const fetchThreadPosts = (threadId) => async dispatch => {
-  const response = await forum.get(`http://localhost:8000/api/posts/?thread=${threadId}`);
-
-  dispatch({ type: FETCH_THREAD_POSTS, payload: response.data })
+export const fetchPostsByThread = (threadId) => async dispatch => {
+  dispatch({ type: FETCH_POSTS_BY_THREAD_PENDING })
+  try {
+    const response = await forum.get(`http://localhost:8000/api/posts/?thread=${threadId}`);
+    dispatch({ type: FETCH_POSTS_BY_THREAD_FULFILLED, payload: response.data })    
+  } catch(err) {
+    dispatch({ type: FETCH_POSTS_BY_THREAD_ERRORS, payload: err })
+  }
 }
 
 // export const createPost = (formValues, threadId, userId) => async dispatch => {
