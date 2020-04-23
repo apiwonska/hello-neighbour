3d6e73eefe490146c26abd8e68e23eb801526c60
anna <annapiwonska1@gmail.com>
2020-04-23 10:55:11 +0200

    Rework reducers to consider all new actions.

--- forumproject/forumproject-ui/src/redux/reducers/postsByThreadReducer.js
+++ forumproject/forumproject-ui/src/redux/reducers/postsByThreadReducer.js
@@ -1,11 +1,25 @@
 import {
-  FETCH_THREAD
+  FETCH_POSTS_BY_THREAD_PENDING,
+  FETCH_POSTS_BY_THREAD_FULFILLED,
+  FETCH_POSTS_BY_THREAD_ERRORS,
 } from '../actions/types';
 
-const reducer = (state={}, action) => {  
+
+const initialState = {
+  fetching: false,
+  fetched: false,
+  data: [],
+  errors: null,
+}
+
+const reducer = (state=initialState, action) => {  
   switch (action.type) {
-    case FETCH_THREAD:
-      return action.payload;
+    case FETCH_POSTS_BY_THREAD_PENDING:
+      return {...initialState, fetching: true};
+    case FETCH_POSTS_BY_THREAD_FULFILLED:
+      return {...initialState, fetched: true, data: action.payload};
+    case FETCH_POSTS_BY_THREAD_ERRORS:
+      return {...initialState, errors: action.payload};
     default:
       return state;
   }
--- forumproject/forumproject-ui/src/redux/reducers/threadsByCategoryReducer.js
+++ forumproject/forumproject-ui/src/redux/reducers/threadsByCategoryReducer.js
@@ -1,12 +1,28 @@
-import { FETCH_CATEGORY_THREADS } from '../actions/types'
+import {
+  FETCH_THREADS_BY_CATEGORY_PENDING,
+  FETCH_THREADS_BY_CATEGORY_FULFILLED,
+  FETCH_THREADS_BY_CATEGORY_ERRORS,
+} from '../actions/types';
 
-const reducers = (state={}, action) => {
+
+const initialState = {
+  fetching: false,
+  fetched: false,
+  data: [],
+  errors: null,
+}
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_CATEGORY_THREADS:      
-      return action.payload;
+    case FETCH_THREADS_BY_CATEGORY_PENDING:
+      return {...initialState, fetching: true};
+    case FETCH_THREADS_BY_CATEGORY_FULFILLED:
+      return {...initialState, fetched: true, data: action.payload};
+    case FETCH_THREADS_BY_CATEGORY_ERRORS:
+      return {...initialState, errors: action.payload};
     default:
       return state;
   }
 }
 
-export default reducers;
+export default reducer;
