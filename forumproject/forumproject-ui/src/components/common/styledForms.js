a5554f83fd866e4f9674e4c79adc4ff111303703
anna <annapiwonska1@gmail.com>
2020-04-28 23:35:15 +0200

    Add form to the LogIn component

---
+++ forumproject/forumproject-ui/src/components/common/styledForms.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+
+`;
+
+export const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+`;
+
+export const Label = styled.label``;
+
+export const Input = styled.input``;
--- forumproject/forumproject-ui/src/pages/login/index.js
+++ forumproject/forumproject-ui/src/pages/login/index.js
@@ -1,11 +1,66 @@
 import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { reduxForm, Field } from 'redux-form';
+import { Redirect } from 'react-router-dom';
 
-const Login = () => {
+import {
+  Form,
+  FormGroup,
+  Label,
+} from '../../components/common/styledForms';
+import {
+  SubmitButtonSmall
+} from '../../components/common/styledButtons';
+import Spinner from '../../components/common/spinner';
+import { logIn } from '../../redux/actions';
+
+
+class LogIn extends React.Component {
+
+  onSubmit(formProps) {
+    this.props.logIn(formProps);
+  }
+
+  render() {
+    const { processing, authenticated } = this.props.auth;
+
+    if (authenticated) {
+      return <Redirect to='/'/>;
+    }
+
+    if (processing) {
+      return <Spinner/>;
+    }
+
+    return (
+      <div>
+        <h2>Log In</h2>
+        <Form method="post" onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+          <FormGroup>
+            <Label htmlFor="username">Username:</Label>
+            <Field component="input" type="text" name="username" autoComplete="none"/>
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="password">Password:</Label>
+            <Field  component="input" type="password" name="password" autoComplete="none"/>
+          </FormGroup>
+          <SubmitButtonSmall type="submit" value="Log In"/>
+        </Form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
   return (
-    <div>
-      Login
-    </div>
+    {
+      auth: state.auth
+    }
   )
 }
 
-export default Login;
+export default compose(
+    connect(mapStateToProps, { logIn }),
+    reduxForm({ form: 'logIn'})
+  )(LogIn);
