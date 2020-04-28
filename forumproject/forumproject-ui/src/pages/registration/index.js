a393aa8a28fb8b5b268f3c99b04e0f8f7440961c
anna <annapiwonska1@gmail.com>
2020-04-28 23:37:26 +0200

    Add form to the Registration component

--- forumproject/forumproject-ui/src/pages/registration/index.js
+++ forumproject/forumproject-ui/src/pages/registration/index.js
@@ -1,11 +1,74 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { reduxForm, Field } from 'redux-form';
+import { Redirect } from 'react-router-dom';
 
-const Registration = () => {
+import {
+  Form,
+  FormGroup,
+  Label,
+} from '../../components/common/styledForms';
+import {
+  SubmitButtonSmall
+} from '../../components/common/styledButtons';
+import { register } from '../../redux/actions';
+import Spinner from '../../components/common/spinner';
+
+
+class Registration extends React.Component {
+
+  onSubmit(formProps) {
+    this.props.register(formProps);
+  }
+
+  render() {
+    const { processing, authenticated } = this.props.auth;
+
+    if (authenticated) {
+      return <Redirect to='/'/>;
+    }
+
+    if (processing) {
+      return <Spinner/>;
+    }
+
+    return (
+      <div>
+        <h2>Register</h2>
+        <Form method="post" onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+          <FormGroup>
+            <Label htmlFor="username">Username:</Label>
+            <Field component="input" type="text" name="username" />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="email">Email:</Label>
+            <Field component="input" type="text" name="email" />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="password">Password:</Label>
+            <Field  component="input" type="password" name="password"/>
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="password2">Confirm Password:</Label>
+            <Field  component="input" type="password" name="password2"/>
+          </FormGroup>
+          <SubmitButtonSmall type="submit"/>
+        </Form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
   return (
-    <div>
-      Registration
-    </div>
+    {
+      auth: state.auth
+    }
   )
 }
 
-export default Registration;
+export default compose(
+    reduxForm({ form: 'registration'}),
+    connect(mapStateToProps, { register })
+  )(Registration);
