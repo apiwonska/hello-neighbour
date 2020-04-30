72d008cec43b3f2eb5edef32d9166a38caa7a3a7
anna <annapiwonska1@gmail.com>
2020-04-30 11:27:31 +0200

    Set persistent login with cookies

--- forumproject/forumproject-ui/src/redux/reducers/authReducer.js
+++ forumproject/forumproject-ui/src/redux/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import Cookies from 'universal-cookie';
+
 import {
   REGISTER_USER_PENDING,
   REGISTER_USER_FULFILLED,
@@ -8,10 +10,36 @@ import {
   LOGOUT_USER
 } from '../actions/types';
 
+const cookies = new Cookies();
+
+const setCookie = (name, value) => {
+  const cookieExpirationDate = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+  cookies.set(name, value, {
+    domain: 'localhost',
+    path: '/',
+    expires: cookieExpirationDate
+  });
+}
+
+const getTokenFromCookie = () => {
+  const token = cookies.get('Authorization');
+  const user = getUserFromCookie();
+  const noUserCookie = !Object.keys(user).length;
+  // If there is no "User" cookie, 
+  // the user will have to login to get user data
+  if (noUserCookie) return '';
+  return token ? token : '';
+}
+
+const getUserFromCookie = () => {
+  const user = cookies.get('User');
+  return user ? user : {};
+}
+
 const INITIAL_STATE = {
   processing: false,
-  authenticated: '',
-  user: {},
+  authenticated: getTokenFromCookie(),
+  user: getUserFromCookie(),
   errors: []
 };
 
@@ -24,17 +52,21 @@ const reducer = (state=INITIAL_STATE, action) => {
     
     case REGISTER_USER_FULFILLED:
     case LOGIN_USER_FULFILLED:
-      const authenticated = action.payload['token'];
+      const token = action.payload['token'];
       const user = action.payload['user'];
-      return {...INITIAL_STATE, authenticated, user };
+      setCookie('User', JSON.stringify(user));
+      setCookie('Authorization', token);
+      return {...INITIAL_STATE, authenticated: token, user };
     
     case REGISTER_USER_ERRORS:
     case LOGIN_USER_ERRORS:
       return {...INITIAL_STATE, errors: action.payload};
     
     case LOGOUT_USER:
-      // remove Auth cookie
+      cookies.remove('Authorization');
+      cookies.remove('User');
       return {...INITIAL_STATE}
+
     default:
       return state;
   }
