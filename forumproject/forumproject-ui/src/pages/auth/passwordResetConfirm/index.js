44405ffb6e161c589ce672e0e195320a8c770674
anna <annapiwonska1@gmail.com>
2020-05-05 12:05:34 +0200

    Add passwordResetConfirm component

---
+++ forumproject/forumproject-ui/src/pages/auth/passwordResetConfirm/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import queryString from 'query-string';
+
+import {
+  Form,
+  FormGroup,
+  Label
+} from '../../../components/styledForms';
+import {
+  SubmitButtonSmall
+} from '../../../components/styledButtons';
+import Spinner from '../../../components/spinner';
+import { FormError } from '../../../components/errors';
+import { confirmPasswordReset } from '../../../redux/actions';
+import { ThemeConsumer } from 'styled-components';
+
+
+class PasswordResetConfirm extends React.Component {
+
+  onSubmit(formProps) {
+    const values = queryString.parse(this.props.location.search)
+    const token = values.token;
+    const data = {...formProps, token};
+    this.props.confirmPasswordReset(data);
+  }
+
+  renderTokenError() {
+    // Token is outdated
+    const statusError = this.props.passwordReset.passwordErrors['status'];
+    // Token was not passed
+    const tokenError = this.props.passwordReset.passwordErrors['token'];
+
+    if (statusError || tokenError) {
+      return <FormError>The reset password link is not valid</FormError>;
+    }
+  }
+
+  renderFieldError(field) {
+    const error = this.props.passwordReset.passwordErrors[field];
+
+    if (error) {
+      return <FormError>{ error }</FormError>;
+    }
+  }
+  
+  renderSuccessMesage() {
+    const { resetPasswordConfirmed } = this.props.passwordReset;
+    if (resetPasswordConfirmed) {
+      return <p>Password was changed. You can <Link to={'/auth'}>log in</Link> now.</p>;
+    }
+  }
+
+  render() {
+    return(
+      <>
+        <h2>Password Reset Confirm</h2>
+        { this.renderTokenError() }
+        <Form method='post' onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+          <FormGroup>
+            <Label htmlFor="password">Password:</Label>
+            <Field component="input" type="password" name="password" autoComplete="none"/>
+            { this.renderFieldError('password') }
+          </FormGroup>
+          <SubmitButtonSmall type="submit" value="Change Password" />
+        </Form>
+        { this.renderSuccessMesage()}
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return (
+    {
+      passwordReset: state.passwordReset
+    }
+  )
+}
+
+export default compose(
+  connect(mapStateToProps, { confirmPasswordReset }),
+  reduxForm({ form: 'passwordResetConfirm' })
+)(PasswordResetConfirm);
