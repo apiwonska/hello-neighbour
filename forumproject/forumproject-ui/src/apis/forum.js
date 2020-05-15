c7ce0d810fd774b5f9e2dece9e89b907e5950a00
anna <annapiwonska1@gmail.com>
2020-05-15 18:08:06 +0200

    Extract forum axios instance to separate file

---
+++ forumproject/forumproject-ui/src/apis/forum.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import store from '../redux/store';
+
+
+const forum = () => {
+  const instance = axios.create()
+  const token = store.getState().auth.authenticated;
+  const auth_header = token ? `Token ${token}` : '';
+  instance.defaults.headers.common['Authorization'] = auth_header;
+  instance.defaults.headers.post['Content-Type'] = 'application/json';
+  return instance;
+}
+
+export default forum;
