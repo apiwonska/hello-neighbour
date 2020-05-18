969edd9c88b0b6c2b9aa2f27cb5b1c175be2ed1c
anna <annapiwonska1@gmail.com>
2020-05-18 21:06:42 +0200

    Updating userReducer with updateProfile actions

--- forumproject/forumproject-ui/src/redux/reducers/userReducer.js
+++ forumproject/forumproject-ui/src/redux/reducers/userReducer.js
@@ -2,27 +2,37 @@ import {
   FETCH_USER_PENDING,
   FETCH_USER_FULFILLED,
   FETCH_USER_ERRORS,
-} from '../actions/types';
-
+  UPDATE_USER_PENDING,
+  UPDATE_USER_FULFILLED,
+  UPDATE_USER_ERRORS,
+} from "../actions/types";
 
 const INITIAL_STATE = {
   fetching: false,
   fetched: false,
   data: {},
   errors: null,
-}
+  updating: false,
+  updateErrors: null,
+};
 
-const reducer = (state=INITIAL_STATE, action) => {  
+const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_USER_PENDING:
-      return {...INITIAL_STATE, fetching: true};
+      return { ...INITIAL_STATE, fetching: true };
     case FETCH_USER_FULFILLED:
-      return {...INITIAL_STATE, fetched: true, data: action.payload};
+      return { ...state, fetching: false, fetched: true, data: action.payload };
     case FETCH_USER_ERRORS:
-      return {...INITIAL_STATE, errors: action.payload};
+      return { ...state, fetching: false, errors: action.payload };
+    case UPDATE_USER_PENDING:
+      return { ...state, updating: true };
+    case UPDATE_USER_FULFILLED:
+      return { ...state, updating: false, data: action.payload };
+    case UPDATE_USER_ERRORS:
+      return { ...state, updating: false, updateErrors: action.payload };
     default:
       return state;
   }
-}
+};
 
 export default reducer;
