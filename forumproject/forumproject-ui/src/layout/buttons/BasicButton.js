bcd2b2c06438271c3cd9431f988fc9fc2a33dc8f
apiwonska <annapiwonska1@gmail.com>
2020-07-31 13:06:25 +0200

    Modify Button and BasicButton layout components

--- forumproject/forumproject-ui/src/layout/buttons/BasicButton.js
+++ forumproject/forumproject-ui/src/layout/buttons/BasicButton.js
@@ -3,30 +3,30 @@ import theme from 'layout/theme';
 
 export default styled.button`
   min-width: 15rem;
-  min-height: 3rem;
+  min-height: 3.5rem;
   font-size: 1.6rem;
   font-family: ${theme.fonts.default};
   line-height: 1.45;
   padding: 0 2rem;
   background-color: ${theme.colors.white};
-  color: ${theme.colors.main};
-  border-color: ${theme.colors.main};
+  color: #374350;
+  border-color: #4d5863;
   border-style: solid;
   border-width: 0.2rem;
-  transform: translateY(0);
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
   transition: 0.3s;
 
   &:disabled,
   &:disabled:hover {
-    color: gray;
-    border-color: gray;
+    color: #a6abb1;
+    border-color: #a6abb1;
     background-color: ${theme.colors.white};
   }
 
   &:hover {
     cursor: pointer;
-    background-color: ${theme.colors.main};
-    color: ${theme.colors.white};
-    transform: translateY(-0.1rem);
+    color: #000;
+    border-color: ${theme.colors.black};
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.4);
   }
 `;
--- forumproject/forumproject-ui/src/layout/buttons/Button.js
+++ forumproject/forumproject-ui/src/layout/buttons/Button.js
@@ -5,19 +5,22 @@ import BasicButton from './BasicButton';
 
 const ButtonWithSize = styled(BasicButton)`
   min-width: ${({ size }) => {
-    if (size === 'S') return '15rem';
+    if (size === 'S') return '10rem';
+    if (size === 'M') return '15rem';
     if (size === 'L') return '20rem';
     if (size === 'XL') return '25rem';
     return null;
   }};
   min-height: ${({ size }) => {
     if (size === 'S') return '3rem';
+    if (size === 'M') return '3.5rem';
     if (size === 'L') return '4rem';
     if (size === 'XL') return '5rem';
     return null;
   }};
   font-size: ${({ size }) => {
-    if (size === 'S') return '1.6rem';
+    if (size === 'S') return '1.4rem';
+    if (size === 'M') return '1.6rem';
     if (size === 'L') return '2rem';
     if (size === 'XL') return '2.4rem';
     return null;
@@ -28,7 +31,7 @@ const yellowButton = `
   background-color: ${theme.colors.secondary};
   color: ${theme.colors.white};
   border: none;
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
   text-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
 
   &:hover {
@@ -42,9 +45,46 @@ const yellowButton = `
   }
 `;
 
+const blueButton = `
+  background-color: ${theme.colors.main};
+  color: ${theme.colors.white};
+  border: none;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+  text-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
+  &:hover {
+    background-color: #275999;
+    color: ${theme.colors.white};
+  }
+
+  &:disabled, &:disabled:hover {
+    background-color: #7CA6DE;
+    color: ${theme.colors.white};
+  }
+`;
+
+const blueOutlineButton = `
+  background-color: ${theme.colors.white};
+  color: ${theme.colors.main};
+  border: .2rem solid ${theme.colors.main};
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+  
+  &:hover {
+    color: #275999;
+    border: .2rem solid #275999;
+  }
+
+  &:disabled, &:disabled:hover {
+    background-color: #7CA6DE;
+    color: ${theme.colors.white};
+  }
+`;
+
 const ButtonWithColor = styled(ButtonWithSize)`
   ${({ color }) => {
     if (color === 'yellow') return yellowButton;
+    if (color === 'blue') return blueButton;
+    if (color === 'blueOutline') return blueOutlineButton;
     return null;
   }}
 `;
