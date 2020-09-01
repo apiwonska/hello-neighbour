6b3da1ffcbb791d22012e72bc35b12364d3b7f01
apiwonska <annapiwonska1@gmail.com>
2020-09-01 12:58:01 +0200

    Modify hoc for error handling

--- forumproject/forumproject-ui/src/shared/hoc/withHandleErrors.jsx
+++ forumproject/forumproject-ui/src/shared/hoc/withHandleErrors.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
-import { DefaultError } from 'layout';
+import { DefaultError, Error404, ServerError } from 'layout';
+
+const isClientError = (errors) => {
+  const statusCode = errors.status.toString();
+  return /^4/.test(statusCode);
+};
+
+const isServerError = (errors) => {
+  const statusCode = errors.status.toString();
+  return /^5/.test(statusCode);
+};
 
 const withHandleErrors = (Cmp) => {
   const Wrapped = ({ errors, ...passThroughProps }) => {
-    if (errors) {
+    if (!_.isEmpty(errors) && isServerError(errors)) {
+      return <ServerError />;
+    }
+
+    if (!_.isEmpty(errors) && isClientError) {
+      if (errors.status === 404) return <Error404 />;
       return <DefaultError />;
     }
+
     return <Cmp {...passThroughProps} />;
   };
 
   Wrapped.propTypes = {
-    errors: PropTypes.bool.isRequired,
+    errors: PropTypes.shape({ status: PropTypes.number }).isRequired,
   };
 
   return Wrapped;
